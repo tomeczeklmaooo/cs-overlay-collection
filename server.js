426652896c68a9dd44a34c176f24f053265c495a
tomeczeklmaooo <tomeczeklmao@proton.me>
2025-07-04 21:54:39 +0200

    begin configuration panel

--- server.js
+++ server.js
@@ -42,10 +42,17 @@ app.get('/break', (req, res) => {
 	console.log(`[${timestamp.toLocaleTimeString('en-GB')}] GET: /break -> break.html`);
 });
 
+app.get('/panel', (req, res) => {
+	const timestamp = new Date();
+	res.sendFile(path.join(__dirname, 'public', 'panel.html'));
+	console.log(`[${timestamp.toLocaleTimeString('en-GB')}] GET: /panel -> panel.html`);
+});
+
 app.use(express.static(path.join(__dirname, 'public')));
 
 server.listen(port, () => {
 	console.log(`Server listening on http://localhost:${port}`);
 	console.log(`HUD available at http://localhost:${port}/hud`);
 	console.log(`Break scene available at http://localhost:${port}/break`);
+	console.log(`Configuration panel available at http://localhost:${port}/panel`);
 });
