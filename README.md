ce80ddb22ebf4dedaec75fc33b3600aa59792ade
tomeczeklmaooo <tomeczeklmao@proton.me>
2025-07-05 17:52:44 +0200

    do electron things

--- README.md
+++ README.md
@@ -6,9 +6,11 @@
 ## Setup
 Copy `gamestate_integration_toasthud.cfg` into `CS2_INSTALL_DIR/game/csgo/cfg`
 
-Run `npm install express ws` in this directory
+Run `npm install express ws electron` in this directory
 
-Run `node server.js` from commandline
+Run `node server.js` from commandline to start the server
+
+Run `npm start` to start the HUD on your screen
 
 Add a browser source in OBS with the link you see in commandline (make sure custom CSS is empty)
 
---
+++ main.js
@@ -0,0 +1,53 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+const url = require('url');
+
+let main_window;
+
+function create_window()
+{
+	main_window = new BrowserWindow({
+		width: 1920,
+		height: 1080,
+		x: 0,
+		y: 0,
+		frame: false,
+		transparent: true,
+		alwaysOnTop: true,
+		resizable: false,
+		fullscreenable: false,
+		skipTaskbar: true,
+		webPreferences: {
+			nodeIntegration: true,
+			contextIsolation: true
+		}
+	});
+
+	main_window.loadURL(url.format({
+		pathname: path.join(__dirname, 'public', 'hud.html'),
+		protocol: 'file:',
+		slashes: true
+	}));
+
+	main_window.setIgnoreMouseEvents(true, { forward: true });
+
+	main_window.on('closed', () => {
+		main_window = null;
+	});
+}
+
+app.whenReady().then(create_window);
+
+app.on('window-all-closed', () => {
+	if (process.platform !== 'darwin')
+	{
+		app.quit();
+	}
+});
+
+app.on('activate', () => {
+	if (BrowserWindow.getAllWindows().length === 0)
+	{
+		create_window();
+	}
+});
